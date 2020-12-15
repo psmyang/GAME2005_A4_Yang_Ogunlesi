5128863e4e26ff159a0322cf1e71a1fad48e7e5a
Miaraii25 <gbogbomia@gmail.com>
2020-12-14 21:15:16 -0500

    Added StartScene

---
+++ Assets/_Scripts/SceneManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+   public void GoToScene(string scene)
+
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+}
